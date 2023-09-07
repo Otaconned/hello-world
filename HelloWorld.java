6e55728ebc00d1739a31ba4807d52f9b83e106f2
Otaconned <sharrelson0429@gmail.com>
2023-09-06 22:17:04 -0400

    Update HelloWorld Java
    > Added method for print statement(s)
    > Added 'program modified' print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,8 @@
 public class HelloWorld
 {
-   system.out.print("Hello World!");
+   public static void main(String[] args)
+   {
+      System.out.print("Hello World!");
+      System.out.print("Program modified, but able to revert to original version using GitHub Desktop.");
+   }
 }
